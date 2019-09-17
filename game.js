3d41f41650a0f443f94c58bc6f2ad4568e520e59
Aidan Saunders <adsaun19@harrisonburg.k12.va.us>
2019-09-17 13:32:59 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,13 +2,20 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+
+createButton('Button', btnPress);
+createButton('- Button', btnPress2); // Make a button
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
+setInterval(btnPress2, 1000);
 
 function btnPress() {
   score++;
   postToPage(score);
 }
 
+function btnPress2() {
+  score--;
+  postToPage(score);
+}
