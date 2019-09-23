d4d30f8637639e38634d5b8be8e40113de1e2d97
Aidan Saunders <adsaun19@harrisonburg.k12.va.us>
2019-09-23 14:31:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 var score = 0; // Create a variable to store the score in
 
-var title = new Title('Clicker'); // Create a title element
-var button = new Button('Button', btnPress);   // Create a button element
-var hidden = new Button('Hello!');
-var button1 = new Button('-button', buttonPress)
+var title = new Title('Forrest'); // Create a title element
+var button = new Button('Wood', btnPress);   // Create a button element
+var hidden = new Button('Build Fire', buttonPress);
+
 
 
 hidden.hide();
@@ -12,32 +12,28 @@ var scr = new Text(score);                    // Create a text element
   // Set up a loop
 
 function btnPress() {
-  score++;     // Increase the score
+  score = score + 1;     // Increase the score
   scr.edit(score); // Update the page with the new score
   if(score > 10) {
     hidden.show();
   }
-  if(score > 15) {
+  if(score < 10) {
     hidden.hide(500);
   }
 }
 
 function buttonPress() {
- score--;
+ score=score-10;
+ scr.edit(score);
+ if(score < 10) {
+   hidden.hide(500);
+ }
 }
+
 
 
 
 function buttonWorld() {
   score--;
   postToPage(score);
-}
-
-var text = new Text ('Press the button to make the number go up or down.');
-
-  if (score > 10){
-  var text = new Text ('Ten');
-}
-if (score > 100){
-var text = new Text ('One Hundred');
 }
